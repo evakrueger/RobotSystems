9d89a1aa55325d1d57302035316f579119794d80
eva415 <eva415@gmail.com>
2025-01-17 12:07:04 -0800

    testing turn on improved function, added angle temp to test

--- picarx/picarx_improved.py
+++ picarx/picarx_improved.py
@@ -88,7 +88,7 @@ class Picarx(object):
         self.cali_dir_value = self.config_flie.get("picarx_dir_motor", default_value="[1, 1]")
         self.cali_dir_value = [int(i.strip()) for i in self.cali_dir_value.strip().strip("[]").split(",")]
         self.cali_speed_value = [0, 0]
-        self.dir_current_angle = 20
+        self.dir_current_angle = 0
         # init pwm
         for pin in self.motor_speed_pins:
             pin.period(self.PERIOD)
@@ -293,6 +293,7 @@ class Picarx(object):
 
 if __name__ == "__main__":
     px = Picarx()
-    px.forward(100)
+    px.set_dir_servo_angle(10)
+    px.forward(50)
     time.sleep(1)
     px.stop()
