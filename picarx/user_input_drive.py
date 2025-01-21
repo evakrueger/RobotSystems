b20a524c824198f703efbdf0b9fa1adf68845290
eva415 <eva415@gmail.com>
2025-01-21 10:59:33 -0800

    testing user input file

--- picarx/user_input_drive.py
+++ picarx/user_input_drive.py
@@ -1,19 +1,20 @@
 from picarx_improved import Picarx
 
-px = Picarx()
-while True:
-    user_command = input(f"Which function would you like to complete?\n1. Forward\n2. Backward\n3. Parallel parking\n4. K=turning\n\n")
-    if user_command == "1" or user_command == "2":
-        user_angle = int(input("At what angle would you like to drive? "))
-        if user_command == "1":
-            px.move_forward_with_steering(25, user_angle, 2)
-        elif user_command == "2":
-            px.move_backward_with_steering(25, user_angle, 2)
-    elif user_command == "3" or user_command == "4":
-        user_direction = input("Which direction would you like to park/k-turn? ")
-        if user_command == "3":
-            px.parallel_park(user_direction)
-        elif user_command == "3":
-            px.k_turn(user_direction)
-    else:
-        break
+if __name__ == "__main__":
+    px = Picarx()
+    while True:
+        user_command = input(f"Which function would you like to complete?\n1. Forward\n2. Backward\n3. Parallel parking\n4. K=turning\n\n")
+        if user_command == "1" or user_command == "2":
+            user_angle = int(input("At what angle would you like to drive? "))
+            if user_command == "1":
+                px.move_forward_with_steering(25, user_angle, 2)
+            elif user_command == "2":
+                px.move_backward_with_steering(25, user_angle, 2)
+        elif user_command == "3" or user_command == "4":
+            user_direction = input("Which direction would you like to park/k-turn? ")
+            if user_command == "3":
+                px.parallel_park(user_direction)
+            elif user_command == "3":
+                px.k_turn(user_direction)
+        else:
+            break
