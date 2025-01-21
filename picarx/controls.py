cb8ce5afe29b0530e5b865290ef3fe07c7fcf725
eva415 <eva415@gmail.com>
2025-01-21 13:31:28 -0800

    testing sensing class

--- picarx/controls.py
+++ picarx/controls.py
@@ -13,7 +13,7 @@ class Sensing():
         return self.px.get_grayscale_data()
 
 class Interpretation():
-    def __init__(self, sensitivity=50, polarity="darker"): # sensitivity and polarity should have default values
+    def __init__(self, sensitivity=100, polarity="darker"): # sensitivity and polarity should have default values
         self.sensitivity = sensitivity
         self.polarity = polarity
     
@@ -24,27 +24,13 @@ values (indicative of an edge), and then using the edge location and sign to det
 whether the system is to the left or right of being centered, and whether it is very off-center
 or only slightly off-center. Make this function robust to different lighting conditions, and with
 an option to have the “target” darker or lighter than the surrounding floor."""
-        # calculate differences between the 3 sensors
-        differences = [
-            grayscale_data[i + 1] - grayscale_data[i]
-            for i in range(len(grayscale_data) - 1)
-        ]
-        if self.polarity == "darker":
-            differences = [-diff for diff in differences]
+        left_grayscale = grayscale_data[0]
+        center_grayscale = grayscale_data[1]
+        right_grayscale = grayscale_data[2]
+        logging.debug(f"left, center, and right divided by center: {left_grayscale/center_grayscale}, {center_grayscale/center_grayscale}, {right_grayscale/center_grayscale}")
+        
 
-        # find maximum difference and its index
-        max_change = max(differences, key=abs)
-        max_index = differences.index(max_change)
-
-        # Determine edge characteristics
-        is_edge = abs(max_change) > self.sensitivity
-        if is_edge:
-            position = "left" if max_change > 0 else "right"
-            offset = "sharp" if abs(max_change) > 2 * self.sensitivity else "slight"
-            logging.debug(f"position: {position}, offset: {offset}")
-            return {"position": position, "offset": offset}
-        else:
-            return {"position": "center", "offset": "slight"}
+        return {"position": "center", "offset": "slight"}
         
 
 if __name__ == "__main__":
