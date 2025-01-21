0d34d4e61d91a44269b0710647a4cf7ac3d08e2a
eva415 <eva415@gmail.com>
2025-01-21 10:58:29 -0800

    testing user input file

--- picarx/user_input_drive.py
+++ picarx/user_input_drive.py
@@ -4,7 +4,7 @@ px = Picarx()
 while True:
     user_command = input(f"Which function would you like to complete?\n1. Forward\n2. Backward\n3. Parallel parking\n4. K=turning\n\n")
     if user_command == "1" or user_command == "2":
-        user_angle = input("At what angle would you like to drive? ")
+        user_angle = int(input("At what angle would you like to drive? "))
         if user_command == "1":
             px.move_forward_with_steering(25, user_angle, 2)
         elif user_command == "2":
