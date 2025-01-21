ac7d0c96818267f71cc2b2d9f0dfedcaed7f089e
eva415 <eva415@gmail.com>
2025-01-21 10:23:02 -0800

    debugging parallel park

--- picarx/picarx_improved.py
+++ picarx/picarx_improved.py
@@ -316,10 +316,10 @@ class Picarx(object):
         logging.debug(f"parallel parking: {direction}")
         if direction == 'left':
             self.move_backward_with_steering(speed, -30, 2)
-            self.move_backward_with_steering(speed, 30, 1)
+            self.move_backward_with_steering(speed, 20, 1)
         elif direction == 'right':
             self.move_backward_with_steering(speed, 30, 2)
-            self.move_backward_with_steering(speed, -30, 1)
+            self.move_backward_with_steering(speed, -20, 1)
         self.stop()
 
 if __name__ == "__main__":
