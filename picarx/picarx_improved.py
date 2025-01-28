42324fbd42825a18ed457920eda409c790cd2912
eva415 <eva415@gmail.com>
2025-01-28 12:16:42 -0800

    trying to debug line following

--- picarx/picarx_improved.py
+++ picarx/picarx_improved.py
@@ -301,7 +301,7 @@ class Picarx(object):
         """Moves the car forward in straight line or with different steering angles"""
         logging.debug(f"moving forward: speed: {speed}, angle: {angle}, duration: {duration}")
         self.set_dir_servo_angle(angle)
-        time.sleep(0.001)
+        time.sleep(0.01)
         self.forward(speed)
         time.sleep(duration)
     
