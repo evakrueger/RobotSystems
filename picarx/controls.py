7c93d80859ab826a37a0631583fd80c8fcdffe07
eva415 <eva415@gmail.com>
2025-01-28 12:37:40 -0800

    trying to debug line following

--- picarx/controls.py
+++ picarx/controls.py
@@ -49,10 +49,9 @@ class Controller():
         
     def follow_line(self, car, line_position):
         # logging.debug(f"\tdriving forward at angle: {line_position*self.angle_scale}")
-        # car.set_dir_servo_angle(line_position*self.angle_scale)
-        # car.forward(0)
+        car.set_dir_servo_angle(line_position*self.angle_scale)
+        car.forward(0)
         # car.move_forward_with_steering(speed=20, angle=line_position*self.angle_scale, duration = 0.05)
-        pass
 
 if __name__ == "__main__":
     px_sensing = Sensing()
