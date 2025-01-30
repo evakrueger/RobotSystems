75b46bee28c3b31fb59b6d47b0e09a9bb12694a1
eva415 <eva415@gmail.com>
2025-01-30 09:48:44 -0800

    debugging camera code

--- picarx/controls.py
+++ picarx/controls.py
@@ -6,9 +6,9 @@ import time
 import io
 import numpy as np
 from vilib import Vilib
-logging_format = "%(asctime)s: %(message)s"
-logging.basicConfig(format=logging_format, level=logging.INFO, datefmt="%H:%M:%S")
-logging.getLogger().setLevel(logging.DEBUG)
+# logging_format = "%(asctime)s: %(message)s"
+# logging.basicConfig(format=logging_format, level=logging.INFO, datefmt="%H:%M:%S")
+# logging.getLogger().setLevel(logging.DEBUG)
 
 
 class Sensing():
