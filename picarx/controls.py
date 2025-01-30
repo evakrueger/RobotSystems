d4ebc515c47c2f77ccc13e7b1450bd862ea21f02
eva415 <eva415@gmail.com>
2025-01-30 12:35:25 -0800

    testing busses.py

--- picarx/controls.py
+++ picarx/controls.py
@@ -78,8 +78,9 @@ an option to have the “target” darker or lighter than the surrounding floor.
         largest_contour = max(contours, key=cv2.contourArea)
         centroid = cv2.moments(largest_contour)
         if centroid['m00'] !=0:
-            
             return (int(centroid['m10']/centroid['m00'])-width)/width
+        print("WAAAAH")
+        return 0.0
     
 class Controller():
     def __init__(self, scaling_factor=25):
