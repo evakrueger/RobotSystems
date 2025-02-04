f19cf45ad8db4bde57c263beee0db07cb00c9319
eva415 <eva415@gmail.com>
2025-02-04 13:28:33 -0800

    working on multimodal control

--- picarx/controls.py
+++ picarx/controls.py
@@ -92,7 +92,6 @@ an option to have the “target” darker or lighter than the surrounding floor.
 class SensingUltrasonic():
     def __init__(self, car):
         self.px = car
-        distance_threshold = 40 # value taken from avoiding_obstacles.py example code
         
     def read_distance(self):
         distance = round(self.px.ultrasonic.read(), 2)
@@ -101,27 +100,16 @@ class SensingUltrasonic():
 
 
 class InterpretationUltrasonic():
-    def __init__(self, sensitivity=2.0, polarity=1): # sensitivity and polarity should have default values
-        self.sensitivity = sensitivity
-        self.polarity = polarity # polarity -1 = darker line, 1 = lighter line
+    def __init__(self, distance_threshold=20): # sensitivity and polarity should have default values
+        self.distance_threshold = distance_threshold
     
-    def line_position(self, grayscale_data):
-        if self.polarity == 1:
-            # logging.debug("lighter")
-            grayscale_data = [grayscale_datapoint - min(grayscale_data) for grayscale_datapoint in grayscale_data]
-        elif self.polarity == -1:
-            # logging.debug("darker")
-            grayscale_data = [grayscale_datapoint - max(grayscale_data) for grayscale_datapoint in grayscale_data]
-        left_grayscale, center_grayscale, right_grayscale = [abs(value) for value in grayscale_data]
-        if left_grayscale > right_grayscale:
-            # logging.debug(f"L > R: {(center_grayscale-left_grayscale)/max(left_grayscale, center_grayscale)}")
-            if (center_grayscale-left_grayscale)/max(left_grayscale, center_grayscale) < 0:
-                return self.polarity*(abs((center_grayscale-left_grayscale)/max(left_grayscale, center_grayscale)))
-            return self.polarity*(1 - (center_grayscale-left_grayscale)/max(left_grayscale, center_grayscale))
-        # logging.debug(f"R > L: {(center_grayscale-right_grayscale)/max(right_grayscale, center_grayscale)}")
-        if (center_grayscale-right_grayscale)/max(right_grayscale, center_grayscale) < 0:
-            return self.polarity*((center_grayscale-right_grayscale)/max(right_grayscale, center_grayscale))
-        return self.polarity*(-1 + (center_grayscale-right_grayscale)/max(right_grayscale, center_grayscale))
+    def check_obstacle(self, ultrasonic_data):
+        if ultrasonic_data <= self.distance_threshold:
+            logging.debug("OBSTACLE OBSTACLE OBSTACLE")
+            return True
+        else:
+            logging.debug("NO OBSTACLES")
+            return False
 
 class Controller():
     def __init__(self, scaling_factor=25):
@@ -136,6 +124,9 @@ class Controller():
             car.set_dir_servo_angle(line_position*self.angle_scale)
             car.forward(20)
         # car.move_forward_with_steering(speed=20, angle=line_position*self.angle_scale, duration = 0.05)
+    
+    def stop_motors(self, car):
+        car.forward(0)
     
     def stop(self, car):
         car.stop()
--- picarx/new_buses.py
+++ picarx/new_buses.py
@@ -9,11 +9,14 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(me
 px_sensing = Sensing(camera=True)
 px_sensing_ultrasonic = SensingUltrasonic(car=px_sensing.px)
 px_interpret = Interpretation(sensitivity=2.0, polarity=-1)
+px_interpret_ultrasonic = InterpretationUltrasonic(distance_threshold=20)
 px_controller = Controller()
 
 # message buses
 sensor_bus = Bus(name="Sensor Bus")
+sensor_bus_ultrasonic = Bus(name="Sensor Bus Ultrasonic")
 interpretation_bus = Bus(name="Interpretation Bus")
+interpretation_bus_obstacles = Bus(name="Interpretation Bus Obstacles")
 termination_bus = Bus(name="Termination Bus")
 
 # Define delays
@@ -43,10 +46,24 @@ def interpret_line_position(grayscale_data):
         logging.info(f"ERROR with line position")
         return 0
 
+def interpret_check_obstacles(ultrasonic_data):
+    try:
+        obstacle_present = px_interpret_ultrasonic.check_obstacle(ultrasonic_data)
+        logging.info(f"OBSTACLE PRESENT?: {obstacle_present}")
+        return obstacle_present
+    except Exception as e:
+        logging.info(f"ERROR with obstacle detection, assume True")
+        return True
+
 # Move vehicle based on line position
 def control_vehicle(line_position):
     logging.info(f"CONTROL VEHICLE Moving based on line position: {line_position}")
     px_controller.follow_line(car=px_sensing.px, line_position=line_position)
+
+def control_vehicle_obstacle(obstacle_present):
+    if obstacle_present:
+        logging.info(f"THERES AN OBSTACLE")
+        px_controller.stop_motors(car=px_sensing.px)
 
 # runtime using Timer class
 timer = Timer(
@@ -58,17 +75,17 @@ timer = Timer(
 )
 
 # Use RossROS.py classes
-# sensor_producer = Producer(
-#     producer_function=get_grayscale_data,
-#     output_buses=sensor_bus,
-#     delay=sensor_delay,
-#     termination_buses=termination_bus,
-#     name="Sensor Producer"
-# )
-
 sensor_producer = Producer(
-    producer_function=get_ultrasonic_data,
+    producer_function=get_grayscale_data,
     output_buses=sensor_bus,
+    delay=sensor_delay,
+    termination_buses=termination_bus,
+    name="Sensor Producer"
+)
+
+sensor_producer_ultrasonic = Producer(
+    producer_function=get_ultrasonic_data,
+    output_buses=sensor_bus_ultrasonic,
     delay=sensor_delay,
     termination_buses=termination_bus,
     name="Sensor Producer Ultrasonic"
@@ -83,9 +100,26 @@ line_interpreter = ConsumerProducer(
     name="Line Interpreter"
 )
 
+obstacle_detector = ConsumerProducer(
+    consumer_producer_function= interpret_check_obstacles,
+    input_buses=sensor_bus_ultrasonic,
+    output_buses=interpretation_bus_obstacles,
+    delay=interpreter_delay,
+    termination_buses=termination_bus,
+    name="Line Interpreter"
+)
+
 vehicle_controller = Consumer(
     consumer_function=control_vehicle,
     input_buses=interpretation_bus,
+    delay=controller_delay,
+    termination_buses=termination_bus,
+    name="Vehicle Controller"
+)
+
+vehicle_controller_ultrasonic = Consumer(
+    consumer_function=control_vehicle_obstacle,
+    input_buses=interpretation_bus_obstacles,
     delay=controller_delay,
     termination_buses=termination_bus,
     name="Vehicle Controller"
