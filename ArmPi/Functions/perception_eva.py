b15ec0ca73ca2824d733e96793f294eae67e09c8
eva415 <eva415@gmail.com>
2025-02-20 13:55:22 -0800

    adding my perception code

--- ArmPi/Functions/perception_eva.py
+++ ArmPi/Functions/perception_eva.py
@@ -73,8 +73,8 @@ class ColorDetector:
                 frame_mask = cv2.inRange(img_processed, color_range_eva[self.detect_color][0], color_range_eva[self.detect_color][1])  # Perform bit operations on the original image and mask
                 blue_channel, green_channel, red_channel = cv2.split(img)
                 cv2.imshow('mask blue image', blue_channel)
-                cv2.imshow('mask blue image', green_channel)
-                cv2.imshow('mask blue image', red_channel)
+                cv2.imshow('mask green image', green_channel)
+                cv2.imshow('mask red image', red_channel)
                 opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))  # Remove small noise.
                 closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))  # Fill small holes.
                 contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]  # find the countours
